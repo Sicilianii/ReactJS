6a69078d898f28d4e0215aa8f3df9d04e0b472c8
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2023-10-26 00:09:35 +0300

    123
    123

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -20,6 +20,10 @@ function App(pattern) {
     const [open, setOpen] = React.useState(true);
     const handleClick = () => {setOpen(!open);};
 
+    function addChats() {
+
+    }
+
     return (
     <div className="App">
         <BrowserRouter>
@@ -34,10 +38,16 @@ function App(pattern) {
                 <ListItemButton>
                     <Link to={'/profile'}>Profile</Link>
                 </ListItemButton>
-                <ListItemButton onClick={handleClick}>
+                <ListItemButton onClick={handleClick} className={'necrash'}>
                     <ListItemText primary="Chats" />
                 </ListItemButton>
+                <button className={'crush'} onClick={addChats}>
+                    <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 12H18M12 6V18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
                 <Collapse in={open} timeout="auto" unmountOnExit>
+
                     <List component="div" disablePadding>
                         <Render List={chatList} setList={setchatList}/>
                     </List>
